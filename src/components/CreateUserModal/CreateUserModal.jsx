9651ed4da2382bda689622f6c9c9e8e0156af1e5
ihorchciv <ihorchciv@gmail.com>
2024-11-10 19:17:13 +0100

    assigned the styles to the inputStyle variable

--- src/components/CreateUserModal/CreateUserModal.jsx
+++ src/components/CreateUserModal/CreateUserModal.jsx
@@ -9,6 +9,24 @@ export function CreateUserModal({ setIsOpenCreateUserModal }) {
   const [user, setUser] = useState({ name: "", password: "", email: "" });
   const dispatch = useDispatch();
 
+  const inputStyle = {
+    "&::before": {
+      border: "1.5px solid var(--Input-focusedHighlight)",
+      transform: "scaleX(0)",
+      left: "2.5px",
+      right: "2.5px",
+      bottom: 0,
+      top: "unset",
+      transition: "transform .15s cubic-bezier(0.1,0.9,0.2,1)",
+      borderRadius: 0,
+      borderBottomLeftRadius: "64px 20px",
+      borderBottomRightRadius: "64px 20px",
+    },
+    "&:focus-within::before": {
+      transform: "scaleX(1)",
+    },
+  };
+
   const onChangeHandler = (value, prop) => {
     setUser({ ...user, [prop]: value });
   };
@@ -29,23 +47,7 @@ export function CreateUserModal({ setIsOpenCreateUserModal }) {
             onChange={(e) => onChangeHandler(e.target.value, "name")}
             value={user.name}
             placeholder="Name"
-            sx={{
-              "&::before": {
-                border: "1.5px solid var(--Input-focusedHighlight)",
-                transform: "scaleX(0)",
-                left: "2.5px",
-                right: "2.5px",
-                bottom: 0,
-                top: "unset",
-                transition: "transform .15s cubic-bezier(0.1,0.9,0.2,1)",
-                borderRadius: 0,
-                borderBottomLeftRadius: "64px 20px",
-                borderBottomRightRadius: "64px 20px",
-              },
-              "&:focus-within::before": {
-                transform: "scaleX(1)",
-              },
-            }}
+            sx={inputStyle}
           />
         </div>
         <div className="inp-field">
@@ -53,23 +55,7 @@ export function CreateUserModal({ setIsOpenCreateUserModal }) {
             onChange={(e) => onChangeHandler(e.target.value, "password")}
             value={user.password}
             placeholder="Password"
-            sx={{
-              "&::before": {
-                border: "1.5px solid var(--Input-focusedHighlight)",
-                transform: "scaleX(0)",
-                left: "2.5px",
-                right: "2.5px",
-                bottom: 0,
-                top: "unset",
-                transition: "transform .15s cubic-bezier(0.1,0.9,0.2,1)",
-                borderRadius: 0,
-                borderBottomLeftRadius: "64px 20px",
-                borderBottomRightRadius: "64px 20px",
-              },
-              "&:focus-within::before": {
-                transform: "scaleX(1)",
-              },
-            }}
+            sx={inputStyle}
           />
         </div>
         <div className="inp-field">
@@ -77,23 +63,7 @@ export function CreateUserModal({ setIsOpenCreateUserModal }) {
             onChange={(e) => onChangeHandler(e.target.value, "email")}
             value={user.email}
             placeholder="Email"
-            sx={{
-              "&::before": {
-                border: "1.5px solid var(--Input-focusedHighlight)",
-                transform: "scaleX(0)",
-                left: "2.5px",
-                right: "2.5px",
-                bottom: 0,
-                top: "unset",
-                transition: "transform .15s cubic-bezier(0.1,0.9,0.2,1)",
-                borderRadius: 0,
-                borderBottomLeftRadius: "64px 20px",
-                borderBottomRightRadius: "64px 20px",
-              },
-              "&:focus-within::before": {
-                transform: "scaleX(1)",
-              },
-            }}
+            sx={inputStyle}
           />
         </div>
         <div className="btn-sub">
