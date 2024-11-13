a389a7cdf87088cb6a2c268153cde9cd893aa7b6
ihorchciv <ihorchciv@gmail.com>
2024-11-13 19:35:31 +0100

    changed CreateUserModal.css and CreateUserModal.jsx

--- src/components/CreateUserModal/CreateUserModal.jsx
+++ src/components/CreateUserModal/CreateUserModal.jsx
@@ -9,24 +9,6 @@ export function CreateUserModal({ setIsOpenCreateUserModal }) {
   const [user, setUser] = useState({ name: "", password: "", email: "" });
   const dispatch = useDispatch();
 
-  const inputStyle = {
-    "&::before": {
-      border: "1.5px solid var(--Input-focusedHighlight)",
-      transform: "scaleX(0)",
-      left: "2.5px",
-      right: "2.5px",
-      bottom: 0,
-      top: "unset",
-      transition: "transform .15s cubic-bezier(0.1,0.9,0.2,1)",
-      borderRadius: 0,
-      borderBottomLeftRadius: "64px 20px",
-      borderBottomRightRadius: "64px 20px",
-    },
-    "&:focus-within::before": {
-      transform: "scaleX(1)",
-    },
-  };
-
   const onChangeHandler = (value, prop) => {
     setUser({ ...user, [prop]: value });
   };
@@ -36,40 +18,48 @@ export function CreateUserModal({ setIsOpenCreateUserModal }) {
     setIsOpenCreateUserModal(false);
   };
 
+  const contentItems = {
+    title: "Create a new User",
+    name: "Name",
+    password: "Password",
+    email: "Email",
+  };
+
   return (
     <div className="modal">
       <div className="modal-content">
-        <i onClick={() => setIsOpenCreateUserModal(false)} className="close">
+        <h3>{contentItems.title}</h3>
+        <b onClick={() => setIsOpenCreateUserModal(false)} className="close">
           X
-        </i>
-        <div className="inp-field">
+        </b>
+        <div className="modal-inp-field">
+          <p>{contentItems.name}</p>
           <Input
             onChange={(e) => onChangeHandler(e.target.value, "name")}
             value={user.name}
-            placeholder="Name"
-            sx={inputStyle}
+            placeholder="name"
           />
         </div>
-        <div className="inp-field">
+        <div className="modal-inp-field">
+          <p>{contentItems.password}</p>
           <Input
+            type="password"
             onChange={(e) => onChangeHandler(e.target.value, "password")}
             value={user.password}
-            placeholder="Password"
-            sx={inputStyle}
+            placeholder="password"
           />
         </div>
-        <div className="inp-field">
+        <div className="modal-inp-field">
+          <p>{contentItems.email}</p>
           <Input
             onChange={(e) => onChangeHandler(e.target.value, "email")}
             value={user.email}
-            placeholder="Email"
-            sx={inputStyle}
+            placeholder="johndoe@email.com"
           />
         </div>
         <div className="btn-sub">
           <Button
-            size="md"
-            color="primary"
+            className="modal-sub-btn"
             onClick={onAddUser}
             disabled={!user.name || !user.password || !user.email}
           >
